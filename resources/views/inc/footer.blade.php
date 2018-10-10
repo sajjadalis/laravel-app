b60bf4fb94412bb37eb78dfa8a29faafb7fe9823
Key7i <key7i@yahoo.com>
2018-10-10 07:30:06 +0500

    second commit

---
+++ resources/views/inc/footer.blade.php
@@ -0,0 +1,67 @@
+<footer class="probootstrap-footer">
+    <div class="container">
+        <div class="row mb-5">
+        <div class="col-md-8">
+            <div class="row">
+            <div class="col-lg mb-4"><h2 class="probootstrap-heading"><a href="#">Laravel</a></h2></div>
+            <div class="col-lg">
+                <div class="probootstrap-footer-widget mb-4">
+                <h2 class="probootstrap-heading-2">Company</h2>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="py-2 d-block">About</a></li>
+                    <li><a href="#" class="py-2 d-block">Jobs</a></li>
+                    <li><a href="#" class="py-2 d-block">Press</a></li>
+                    <li><a href="#" class="py-2 d-block">News</a></li>
+                </ul>
+                </div>
+            </div>
+            <div class="col-lg">
+                <div class="probootstrap-footer-widget mb-4">
+                <h2 class="probootstrap-heading-2">Communities</h2>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="py-2 d-block">Support</a></li>
+                    <li><a href="#" class="py-2 d-block">Sharing is Caring</a></li>
+                    <li><a href="#" class="py-2 d-block">Better Web</a></li>
+                    <li><a href="#" class="py-2 d-block">News</a></li>
+                </ul>
+                </div>
+            </div>
+            <div class="col-lg">
+                <div class="probootstrap-footer-widget mb-4">
+                <h2 class="probootstrap-heading-2">Useful links</h2>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="py-2 d-block">Bootstrap 4</a></li>
+                    <li><a href="#" class="py-2 d-block">jQuery</a></li>
+                    <li><a href="#" class="py-2 d-block">HTML5</a></li>
+                    <li><a href="#" class="py-2 d-block">Sass</a></li>
+                </ul>
+                </div>
+            </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="probootstrap-footer-widget mb-4">
+            <ul class="probootstrap-footer-social list-unstyled float-md-right float-lft">
+                <li><a href="#"><span class="icon-twitter"></span></a></li>
+                <li><a href="#"><span class="icon-facebook"></span></a></li>
+                <li><a href="#"><span class="icon-instagram"></span></a></li>
+            </ul>
+            </div>
+        </div>
+        </div>
+        <div class="row">
+        <div class="col-md text-left">
+            <ul class="list-unstyled footer-small-nav">
+            <li><a href="#">Legal</a></li>
+            <li><a href="#">Privacy</a></li>
+            <li><a href="#">Cookies</a></li>
+            <li><a href="#">Terms</a></li>
+            <li><a href="#">About</a></li>
+            </ul>
+        </div>
+        <div class="col-md text-md-right text-left">
+            <p><small>&copy; Laravel App 2018. All Rights Reserved.</p>
+        </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('title')
+    <div class="col-sm">
+        <div class="probootstrap-text">
+            <h1 class="probootstrap-heading text-white mb-4">{{$title}}</h1>
+            <div class="probootstrap-subheading mb-5">
+                <p class="h4 font-weight-normal">This is a about page</p>
+            </div>
+        </div>
+    </div>
+@endsection
+@section('content')
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id lorem in tortor egestas tincidunt quis sit amet est. Sed mattis augue quis nisl fringilla, ac feugiat sem semper. Nam egestas eleifend arcu, sit amet rutrum libero posuere vitae. Vestibulum sed iaculis sem, eget placerat diam. Aenean imperdiet vehicula urna, suscipit aliquam turpis rutrum eu. Donec vulputate libero nisl, vitae sagittis libero tincidunt et. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque lacinia magna vitae neque semper laoreet. Quisque lobortis neque quis nunc fermentum malesuada.</p>
+
+    <p>Donec rutrum, justo at pellentesque pulvinar, justo arcu pellentesque nisi, sed tincidunt diam ex et lectus. In eu mauris posuere, porta erat sit amet, efficitur mi. In elementum cursus neque, id pulvinar tellus. Cras volutpat porta est, vitae fringilla magna bibendum et. Praesent quis rutrum orci. In sit amet tellus nec arcu mollis ultricies. Maecenas id nunc sed lectus dapibus hendrerit. Vivamus facilisis augue sed nibh posuere aliquam. Quisque velit leo, sollicitudin ut vulputate sed, blandit quis enim. Nunc pharetra enim eu turpis tincidunt, eu rhoncus leo placerat. Aenean ac condimentum eros. Vestibulum efficitur nisi purus, in iaculis enim rutrum et. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus maximus vehicula ex quis semper.</p>
+@endsection
